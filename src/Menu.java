c233a4fe1d2d5b79aa0a9157cc4f6a4aad672756
eugenet <eugenetang_@hotmail.com>
2017-02-19 17:52:44 -0800

    Added Menu class and test.

---
+++ src/Menu.java
@@ -0,0 +1,7 @@
+public class Menu {
+    public String welcome() {
+        String welcomeMessage = "===Welcome to Biblioteca!===";
+        System.out.println(welcomeMessage);
+        return welcomeMessage;
+    }
+}
---
+++ test/MenuTest.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MenuTest {
+    Menu menu;
+
+    @Before
+    public void setUp() {
+        menu = new Menu();
+    }
+
+    @Test
+    public void welcome() {
+        assertEquals("===Welcome to Biblioteca!===", menu.welcome());
+    }
+
+}
